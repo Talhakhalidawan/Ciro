6228dfe0775c87a150286632ac36b139675ff826
talhakhalidawan <talhakhalidlkp@gmail.com>
2026-05-21 03:37:10 +0500

    moved staic settings to project urls and django version lowered

--- api/urls.py
+++ api/urls.py
@@ -1,15 +1,10 @@
 from django.urls import path
 from .views.weather import weather_view
 from .views.community import create_incident, list_incidents
-from django.conf import settings
-from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('weather/', weather_view, name='weather'),
     path('community/incidents/', list_incidents, name='list_incidents'),
     path('community/incidents/create/', create_incident, name='create_incident'),
 ]
-
-
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
